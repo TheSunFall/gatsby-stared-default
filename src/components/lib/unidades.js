aa976a9c0b52c495645fe62959126778658a7570
Juan Aguilar <juanpabloaguilar@outlook.com>
2023-11-19 12:01:49 -0500

    working calc #1

--- src/components/lib/unidades.js
+++ src/components/lib/unidades.js
@@ -1,20 +1,171 @@
 const cambios = {
-    longitud: {
-      "metros": 1,
-      "centímetros": 0.01,
-      "kilometro": 1000,
-      "micrómetro": 0.000001,
-      "nanómetro": 0.000000001,
-},
-    energia: {"julios": 1, "calorías": 4.184},
-    densidad: ["g/cm³", "kg/m³", "kg/L", "g/ml"],
-    volumen: ["litro", "milímetro cúbico", "cm³", "m³"],
-    masa: [
-      "gramo",
-      "kilogramo",
-      "miligramo",
-      "centigramo",
-      "decagramo",
-      "hectogramo",
-    ],
-  }
+  a: 1,
+  longitud: {
+    metros: {
+      abv: "m",
+      val: 1,
+      def: "aaaa",
+    },
+    centímetros: {
+      abv: "cm",
+      val: 0.01,
+      def: "",
+    },
+    kilómetros: {
+      abv: "km",
+      val: 100,
+      def: "",
+    },
+    micrómetros: {
+      abv: "µm",
+      val: 0.000001,
+      def: "",
+    },
+    nanómetros: {
+      abv: "µm",
+      val: 0.000000001,
+      def: "",
+    },
+    pulgadas: {
+      abv: "in",
+      val: 0.00254,
+      def: "",
+    },
+    pies: {
+      abv: "ft",
+      val: 0.03048,
+      def: "",
+    },
+    picómetros: {
+      abv: "pm",
+      val: 0.000000000001,
+      def: "",
+    },
+  },
+  masa: {
+    kilogramos: {
+      abv: "kg",
+      val: 1,
+      def: "",
+    },
+    gramos: {
+      abv: "g",
+      val: 0.001,
+      def: "",
+    },
+    libras: {
+      abv: "lb",
+      val: 0.454,
+      def: "",
+    },
+    onzas: {
+      abv: "oz",
+      val: 0.02835,
+      def: ""
+    },
+    slug: {
+      abv: "sl",
+      val: 14.59,
+      def: "",
+    },
+  },
+  fuerza: {
+    newtons: {
+      abv: "N",
+      val: 1,
+      def: "",
+    },
+    "kilogramo-fuerza": {
+      abv: "kg-f",
+      val: 9.81,
+      def: "",
+    },
+    "libra-fuerza": {
+      abv: "lbf",
+      val: 4.448,
+      def: "",
+    },
+    dina: {
+      abv: "dyn",
+      val: 0.0000001,
+      def: "",
+    },
+  },
+  energia: {
+    joules: {
+      abv: "J",
+      val: 1,
+      def: "",
+    },
+    calorías: {
+      abv: "cal",
+      val: 4.184,
+      def: "",
+    },
+    kilográmetros: {
+      abv: "kgm",
+      val: 9.81,
+      def: "",
+    },
+    ergios: {
+      abv: "erg",
+      val: 0.0000001,
+      def: "",
+    },
+  },
+  potencia: {
+    watts: {
+      abv: "W",
+      val: 1,
+      def: "",
+    },
+    "caballos de fuerza": {
+      abv: "HP",
+      val: 745.7,
+      def: "",
+    },
+    "caballos de vapor": {
+      abv: "CV",
+      val: 735.7,
+      def: "",
+    },
+    "kilocalorías/hora": {
+      abv: "kcal/h",
+      val: 1.163,
+      def: "",
+    },
+  },
+  volumen: {
+    litro: {
+      abv: "L",
+      val: 1,
+      def: "",
+    },
+    "centímetro cúbico": {
+      abv: "cm³",
+      val: 0.001,
+      def: "",
+    },
+    "metro cúbico": {
+      abv: "m³",
+      val: 1000,
+      def: "",
+    },
+    "galón": {
+      abv: "gal",
+      val: 4.546,
+      def: "",
+    },
+    "barril": {
+      abv: "barril",
+      val: 159,
+      def: "",
+    },
+    "onza líquida": {
+      abv: "oz",
+      val: 0.02957,
+      def: "",
+    },
+  },
+}
+export default cambios
--- src/pages/calcunidades.js
+++ src/pages/calcunidades.js
@@ -1,30 +1,207 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import "../components/calcunidades.css"
 import { useState } from "react"
+import "../components/calcunidades.css"
 
+
+const cambios = {
+  a: {n:1,v:2},
+  longitud: {
+    metros: {
+      abv: "m",
+      val: 1,
+      def: "aaaa",
+    },
+    centímetros: {
+      abv: "cm",
+      val: 0.01,
+      def: "",
+    },
+    kilómetros: {
+      abv: "km",
+      val: 100,
+      def: "",
+    },
+    micrómetros: {
+      abv: "µm",
+      val: 0.000001,
+      def: "",
+    },
+    nanómetros: {
+      abv: "µm",
+      val: 0.000000001,
+      def: "",
+    },
+    pulgadas: {
+      abv: "in",
+      val: 0.00254,
+      def: "",
+    },
+    pies: {
+      abv: "ft",
+      val: 0.03048,
+      def: "",
+    },
+    picómetros: {
+      abv: "pm",
+      val: 0.000000000001,
+      def: "",
+    },
+  },
+  masa: {
+    kilogramos: {
+      abv: "kg",
+      val: 1,
+      def: "",
+    },
+    gramos: {
+      abv: "g",
+      val: 0.001,
+      def: "",
+    },
+    libras: {
+      abv: "lb",
+      val: 0.454,
+      def: "",
+    },
+    onzas: {
+      abv: "oz",
+      val: 0.02835,
+      def: ""
+    },
+    slug: {
+      abv: "sl",
+      val: 14.59,
+      def: "",
+    },
+  },
+  fuerza: {
+    newtons: {
+      abv: "N",
+      val: 1,
+      def: "",
+    },
+    "kilogramo-fuerza": {
+      abv: "kg-f",
+      val: 9.81,
+      def: "",
+    },
+    "libra-fuerza": {
+      abv: "lbf",
+      val: 4.448,
+      def: "",
+    },
+    dina: {
+      abv: "dyn",
+      val: 0.0000001,
+      def: "",
+    },
+  },
+  energia: {
+    joules: {
+      abv: "J",
+      val: 1,
+      def: "",
+    },
+    calorías: {
+      abv: "cal",
+      val: 4.184,
+      def: "",
+    },
+    kilográmetros: {
+      abv: "kgm",
+      val: 9.81,
+      def: "",
+    },
+    ergios: {
+      abv: "erg",
+      val: 0.0000001,
+      def: "",
+    },
+  },
+  potencia: {
+    watts: {
+      abv: "W",
+      val: 1,
+      def: "",
+    },
+    "caballos de fuerza": {
+      abv: "HP",
+      val: 745.7,
+      def: "",
+    },
+    "caballos de vapor": {
+      abv: "CV",
+      val: 735.7,
+      def: "",
+    },
+    "kilocalorías/hora": {
+      abv: "kcal/h",
+      val: 1.163,
+      def: "",
+    },
+  },
+  volumen: {
+    litro: {
+      abv: "L",
+      val: 1,
+      def: "",
+    },
+    "centímetro cúbico": {
+      abv: "cm³",
+      val: 0.001,
+      def: "",
+    },
+    "metro cúbico": {
+      abv: "m³",
+      val: 1000,
+      def: "",
+    },
+    "galón": {
+      abv: "gal",
+      val: 4.546,
+      def: "",
+    },
+    "barril": {
+      abv: "barril",
+      val: 159,
+      def: "",
+    },
+    "onza líquida": {
+      abv: "oz",
+      val: 0.02957,
+      def: "",
+    },
+  },
+}
 const Form = () => {
   const [tipo, setTipo] = useState("longitud")
   const [uIn, setUIn] = useState("metros")
   const [uDest, setUDest] = useState("centímetros")
+  const [valor, setValor] = useState(0)
   const unidades = {
     longitud: [
       "metros",
-      "micrómetro",
-      "nanómetro",
-      "picometro",
+      "centímetros",
+      "kilómetros",
+      "pulgadas",
+      "pies",
+      "micrómetros",
+      "nanómetros",
+      "picometros",
     ],
-    energia: ["julios", "calorías"],
-    densidad: ["g/cm³", "kg/m³", "kg/L", "g/ml"],
-    volumen: ["litro", "milímetro cúbico", "cm³", "m³"],
+    fuerza: ["newtons", "kilogramos-fuerza","libras-fuerza", "dinas"],
+    energia: ["joules", "calorías", "kilográmetros","ergios"],
+    potencia: ["watts", "caballos de fuerza","caballos de vapor","kilocalorías/hora"],
+
+    volumen: ["litros", "centímetros cúbico", "metros cúbico", "galones", "barriles"],
     masa: [
-      "gramo",
-      "kilogramo",
-      "miligramo",
-      "centigramo",
-      "decagramo",
-      "hectogramo",
+      "gramos",
+      "kilogramos",
+      "libras",
+      "onzas",
+      "slug"
     ],
   }
   return (
@@ -33,20 +210,23 @@ const Form = () => {
       <select
         id="tipoMedida"
         value={tipo}
-        onChange={e => setTipo(e.target.value)}
+        onChange={(e) => {
+          setTipo(e.target.value);
+          setUIn(unidades[e.target.value][0]);
+            setUDest(unidades[e.target.value][0]);
+        }}
       >
         <option value="longitud">Longitud</option>
-        <option value="energia">Energía</option>
-        <option value="densidad">Densidad</option>
-        <option value="volumen">Volumen</option>
         <option value="masa">Masa</option>
-        <option value="temperatura">Temperatura</option>
-        <option value="presion">Presión</option>
-        <option value="tiempo">Tiempo</option>
+        <option value="fuerza">Fuerza</option>
+        <option value="energia">Energía</option>
+        <option value="potencia">Potencia</option>
+        <option value="volumen">Volumen</option>
       </select>
 
       <label htmlFor="valor">Ingrese el valor:</label>
-      <input type="number" id="valor" />
+      <input type="number" id="valor" value={valor}
+        onChange={e => setValor(e.target.value)}/>
 
       <label htmlFor="unidadOrigen">Unidad de origen:</label>
       <select id="unidadOrigen" value={uIn}
@@ -55,15 +235,16 @@ const Form = () => {
           return <option value={u}>{u}</option>
         })}
       </select>
-
       <label htmlFor="unidadDestino">Unidad de destino:</label>
       <select id="unidadDestino" value={uDest}
         onChange={e => setUDest(e.target.value)}>
-        {unidades[tipo].map(u => {
-          return <option value={u}>{u}</option>
+        {unidades[tipo].map(un => {
+          return <option value={un}>{un}</option>
         })}
       </select>
-      <button>Convertir</button>
+
+      <div id="resultado">El resultado es {valor*cambios[tipo][uDest].val/cambios[tipo][uIn].val}</div>
+      {console.log(cambios[tipo][uIn].val)}
     </div>
   )
 }
@@ -73,7 +254,7 @@ const SecondPage = () => (
     <h1>Convertidor de Medidas</h1>
     <Form></Form>
 
-    <div id="resultado"></div>
+    
   </Layout>
 )
 
